2b0c5dfe3c3cff4ec1e3a5b2906f387ad9bf2f16
Soph1514 <116025561+Soph1514@users.noreply.github.com>
2025-03-28 10:30:40 +0000

    Actions: Documentation for

---
+++ docs/trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["conjure_core",[["impl ValueEnum for <a class=\"enum\" href=\"conjure_core/pro_trace/enum.VerbosityLevel.html\" title=\"enum conjure_core::pro_trace::VerbosityLevel\">VerbosityLevel</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[190]}
